efbe4932d476ab53b287678de120ae877d0c4578
Jay Miller <kjaymiller@gmail.com>
2022-07-29 14:54:21 -0700

    add capture file and test

---
+++ src/capture.py
@@ -0,0 +1,20 @@
+"""Capture Video from different sources"""
+
+def from_camera(source):
+    """Use a Camera as Input"""
+    pass
+
+
+def from_device(source):
+    """Use a Capture Device as Input"""
+    pass
+
+
+def from_file(source):
+    """Use a File as Input"""
+    pass
+
+
+def from_url(source):
+    """Use a URL as Input"""
+    pass
